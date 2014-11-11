172e68813c0c53fea354e01f1bd9991cea2ecf84
Matt Wittmann <mcwitt@gmail.com>
2014-11-11 22:06:05 +0100

    Add log-spaced correlation measurements.

--- src/mod_corr.c
+++ src/mod_corr.c
@@ -16,7 +16,7 @@ static void print_header(FILE *fp)
 void mod_corr_init(mod_corr_t *self, FILE *fp)
 {
     self->cr = (double*) fftw_malloc(sizeof(double) * N);
-    self->ck = (complex*) fftw_malloc(sizeof(complex) * N);
+    self->ck = (complex double*) fftw_malloc(sizeof(complex double) * N);
 
     self->dft = fftw_plan_dft_r2c_1d(N, self->cr, self->ck, FFTW_MEASURE);
 
@@ -28,14 +28,14 @@ void mod_corr_init(mod_corr_t *self, FILE *fp)
 
 void mod_corr_reset(mod_corr_t *self)
 {
-    memset(self->C4, 0, sizeof self->C4);
+    memset(self->C4, 0., sizeof self->C4);
 }
 
 static void accumulate(const int S1[],
                        const int S2[],
                        double C4[],
                        double cr[],
-                       complex ck[],
+                       complex double ck[],
                        fftw_plan dft,
                        fftw_plan idct)
 {
@@ -46,7 +46,7 @@ static void accumulate(const int S1[],
     /* compute the overlap field */
     for (i = 0; i < N; i++) cr[i] = (1./N)*S1[i]*S2[i];
 
-    /* FT to k space and square */
+    /* FT to k-space and square */
     fftw_execute(dft);
     for (i = 0; i <= N/2; i++) cr[i] = pow(cabs(ck[i]), 2);
 
--- src/mod_corr.h
+++ src/mod_corr.h
@@ -2,14 +2,13 @@
 #include "sample.h"
 #include <complex.h>
 #include <fftw3.h>
-#include <stdio.h>
 
 typedef struct
 {
     double C4[NUM_REPLICAS][N/2];
 
     double *cr;
-    complex *ck;
+    complex double *ck;
     fftw_plan dft, idct;
 
 } mod_corr_t;
